95e1def6266fe534d2ab119d3ba4a2049bc48696
YinaePark <dlsoso1@unist.ac.kr>
2025-09-06 17:04:05 +0900

    chore: add new entity to data-source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import { GameEntity } from '../entities/GameEntity';
 import { PlayerEntity } from '../entities/PlayerEntity';
-
+import { UserEntity } from '../entities/UserEntity'; 
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
@@ -20,7 +20,7 @@ export const AppDataSource = new DataSource(
         url: connectionString,
         synchronize: true,
         logging: true,
-        entities: [GameEntity, PlayerEntity],
+        entities: [GameEntity, PlayerEntity, UserEntity],
         migrations: ['src/migrations/*.{ts,js}'],
         migrationsRun: true,
       }
@@ -33,7 +33,7 @@ export const AppDataSource = new DataSource(
         database,
         synchronize: true,
         logging: true,
-        entities: [GameEntity, PlayerEntity],
+        entities: [GameEntity, PlayerEntity, UserEntity],
         migrations: ['src/migrations/*.{ts,js}'],
         migrationsRun: true,
       }
